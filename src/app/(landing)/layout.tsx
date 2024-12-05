323cb730c1c1b4162edcf1beace2dbd9d23ff025
alexgutscher26 <madajoe6969@gmail.com>
2024-12-04 23:08:42 -0600

    feat(landing): add Footer component to layout

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -1,11 +1,13 @@
 import { ReactNode } from "react"
 import { Navbar } from "@/components/navbar"
+import { Footer } from "@/components/footer"
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <>
       <Navbar />
       {children}
+      <Footer />
     </>
   )
 }
